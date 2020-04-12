0f598b5c4e18018b177da374bdbf406d0ac2cc43
Pawel <p.iwan@f5.com>
2020-04-12 09:53:39 +0200

    change another option

--- src/imp/openssl.rs
+++ src/imp/openssl.rs
@@ -270,9 +270,10 @@ impl TlsConnector {
                 "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
             )?;
         }
+        connector.set_options(SslOptions::NO_SESSION_RESUMPTION_ON_RENEGOTIATION);
+        #[cfg(any(ossl111, ossl110h))]
+        connector.set_options(SslOptions::NO_RENEGOTIATION);
         // disable renegotiation
-        #[cfg(ossl110h)]
-        connector.set_options(SslOptions::NO_RENEGOTIATION)?;
         for cert in &builder.root_certificates {
             if let Err(err) = connector.cert_store_mut().add_cert((cert.0).0.clone()) {
                 debug!("add_cert error: {:?}", err);
